e4370dfd323a029ce3e769e00974616fe1cd3035
https://github.com/RickyMartin12/moodle_elearning.git <11knum15@gmail.com>
2022-11-21 10:41:00 +0000

    Init Web5

--- components/Works.tsx
+++ components/Works.tsx
@@ -20,11 +20,11 @@ const Works = () => {
     return(
         <>
             <div className="py-8 pb-4 px-12 h-full flex flex-col">
-                <h1 className="text-6xl font-semibold font-header tracking-wide text-center">Ricardo&nbsp;
+                <h1 className="text-4xl font-semibold font-header tracking-wide text-center">Ricardo&nbsp;
                     <span className="text-accent">Peleira</span>
                 </h1>
                 <div className="mt-0 mb-6">      
-                    <h2 className="text-4xl tracking-wider text-center">Full-stack Developer</h2>
+                    <h2 className="text-2xl tracking-wider text-center">Full-stack Developer</h2>
                 </div>
 
                 <div className="mb-6 prose-sm prose-slate">
